894c5330f33bdbeddebdc00d25e522af9bd3c6fa
xandymagnus <alexandre.magno325@gmail.com>
2024-05-29 08:21:55 -0300

    adding "paragraphBreak" function (delete a "\n" and not count the paragraphs)

--- src/index.js
+++ src/index.js
@@ -6,13 +6,22 @@ const filePath = process.argv;
 const link = filePath[2];
 
 fs.readFile(link, 'UTF-8', (erro, text) => {
-    verifyDupiclateWords(text)
+    paragraphBreak(text)
+    // verifyDupiclateWords(text)
 })
 
 // {
 //     "web": 5,
 //     "desktop": 4
 // }
+
+function paragraphBreak (text) {
+    const paragraphs =  text.toLowerCase().split('\n');
+    const count = paragraphs.map((paragraph) => {
+        return verifyDupiclateWords(paragraph); 
+    })
+    console.log(count);
+}
 
 function verifyDupiclateWords (text) {
     const wordList = text.split(' ');
@@ -21,5 +30,5 @@ function verifyDupiclateWords (text) {
     wordList.forEach(word => {
         result[word] = (result[word] || 0) + 1;
     })
-    console.log(result);
+    return result;
 }
