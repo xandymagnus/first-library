1a281edb7418d8305d9be170ed8950c40a036a64
xandymagnus <alexandre.magno325@gmail.com>
2024-06-07 15:59:10 -0300

    pass imports to cli.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-const fs = require('fs');
-
-const handleErrors = require('./error/errorFunctions');
-
-const filePath = process.argv;
-const link = filePath[2];
-
-fs.readFile(link, 'UTF-8', (erro, text) => {
-    try {
-        if (erro) throw erro
-        countWords(text);
-    } catch(erro) {
-        handleErrors(erro);
-    }
-})
-
 function countWords (text) {
     const paragraphs = extractParagraphs(text)
     const count = paragraphs.flatMap((paragraph) => {
