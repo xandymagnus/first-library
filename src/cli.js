3e2f1630d40a6b8a5c812d6fbca3eb69c692de7c
xandymagnus <alexandre.magno325@gmail.com>
2024-06-07 17:03:09 -0300

    add async-await

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,29 @@
 import fs from 'fs';
-import handleErrors from './error/errorFunctions.js'
+import handleErrors from './error/errorFunctions.js';
+import { countWords } from './index.js';
 
 const filePath = process.argv;
 const link = filePath[2];
+const address = filePath[3];
 
 fs.readFile(link, 'UTF-8', (erro, text) => {
     try {
         if (erro) throw erro
-        countWords(text);
+       const result = countWords(text);
+       createAndSaveFile(result, address);
     } catch(erro) {
         handleErrors(erro);
     }
 })
+
+async function createAndSaveFile (wordList, address) {
+    const newFile = `${address}/result.txt`;
+    const textWords = JSON.stringify(wordList);
+
+    try {
+        await fs.promises.writeFile(newFile, textWords);
+        console.log('file created');
+    } catch(erro) {
+        throw erro;
+    }
+}
